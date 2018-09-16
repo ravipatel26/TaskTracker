f96869883ea671e7826c36caf4304fb6570188d0
ivarletap <ravikumarpatel2610@gmail.com>
2018-09-16 12:18:59 -0400

    connecting BE to FE for users

--- Frontend/src/app/components/all-users/all-users.component.ts
+++ Frontend/src/app/components/all-users/all-users.component.ts
@@ -17,17 +17,17 @@ export class AllUsersComponent implements OnInit {
   ngOnInit() {
     // TODO: use this to get all users!!!
     // may need to do cross origin in java (see part 4: https://www.youtube.com/watch?v=v4_QR4PAPsM)
-    // this._userService.getUsers().subscribe((users) => {
-    //   console.log(users);
-    //   this.users = users;
-    // }, error => {
-    //   console.log(error);
-    // })
-    this.users = [
-      {id: 1, firstName: "abc", lastName: "abc", dateOfBirth: new Date('03/22/4007'), username: 'ravster', password: 'red son' },
-      {id: 2, firstName: "xyz", lastName: "xyz", dateOfBirth: new Date('04/22/4007'), username: 'ravster', password: 'red son' },
-      {id: 3, firstName: "mno", lastName: "mno", dateOfBirth: new Date('05/22/4007'), username: 'ravster', password: 'red son' },
-    ];
+    this._userService.getUsers().subscribe((users) => {
+      console.log(users);
+      this.users = users;
+    }, error => {
+      console.log(error);
+    })
+    // this.users = [
+    //   {id: 1, firstName: "abc", lastName: "abc", dateOfBirth: new Date('03/22/4007'), username: 'ravster', password: 'red son' },
+    //   {id: 2, firstName: "xyz", lastName: "xyz", dateOfBirth: new Date('04/22/4007'), username: 'ravster', password: 'red son' },
+    //   {id: 3, firstName: "mno", lastName: "mno", dateOfBirth: new Date('05/22/4007'), username: 'ravster', password: 'red son' },
+    // ];
   }
 
   editUser(user) {
--- UserService/src/main/java/com/ravi/springboot/Controller/UserController.java
+++ UserService/src/main/java/com/ravi/springboot/Controller/UserController.java
@@ -3,6 +3,7 @@ package com.ravi.springboot.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import com.ravi.springboot.Model.User;
 import com.ravi.springboot.Service.UserService;
 
 @RestController
+@CrossOrigin(origins="http://localhost:4200")
 public class UserController {
 
 	@Autowired
