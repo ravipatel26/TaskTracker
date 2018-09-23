ab2f5377109a0785da0cc3c4b5c24e97b0e1869f
unknown <ravikumarpatel2610@gmail.com>
2018-09-22 22:30:08 -0400

    adding unit tests for login service

--- UserService/src/main/java/com/ravi/springboot/Service/LoginService.java
+++ UserService/src/main/java/com/ravi/springboot/Service/LoginService.java
@@ -12,7 +12,7 @@ import com.ravi.springboot.Repository.UserRepository;
 public class LoginService {
 
 	private UserRepository userRepository = new UserRepository();
-	private final String GET_USER_BY_USERNAME = "select * from user where username='%s' and password='%s'";
+	private final String GET_USER_BY_CREDENTIALS = "select * from user where username='%s' and password='%s'";
 	private final String USERNAME_MAP_KEY = "username";
 	private final String PASSWORD_MAP_KEY = "password";
 	
@@ -20,13 +20,13 @@ public class LoginService {
 		if (isInvalidCredential(credentials))
 			return null;
 
-		String query = String.format(GET_USER_BY_USERNAME, credentials.get(USERNAME_MAP_KEY), credentials.get(PASSWORD_MAP_KEY));
+		String query = String.format(GET_USER_BY_CREDENTIALS, credentials.get(USERNAME_MAP_KEY), credentials.get(PASSWORD_MAP_KEY));
 		List<User> users = userRepository.executeRetrieveQuery(query);
 		return users.size() > 0 ? users.get(0) : null;
 	}
 	
 	private boolean isInvalidCredential(Map<String, Object> credentials) {
 		return credentials == null || credentials.isEmpty() ||
-			   !credentials.containsKey(USERNAME_MAP_KEY) || !credentials.containsKey(PASSWORD_MAP_KEY);
+			   credentials.get(USERNAME_MAP_KEY) == null || credentials.get(PASSWORD_MAP_KEY) == null;
 	}
 }
